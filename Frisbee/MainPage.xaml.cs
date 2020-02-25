8789ad840e588248b51c3ff7e7977c77c0958610
Tyler-Carroll <htfu513@gmail.com>
2020-02-25 14:21:11 -0600

    Working Camera Access

--- Frisbee/MainPage.xaml.cs
+++ Frisbee/MainPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
-using System.IO;
+using System.Threading.Tasks;
+using Plugin.Media;
 using Xamarin.Forms;
+
 
 namespace Frisbee
 {
@@ -9,11 +11,21 @@ namespace Frisbee
 
         public MainPage()
         {
-
             InitializeComponent();
 
-
             HelloMessage.Text = "Hello " + App.Username + "!";
+         }
+
+        private async void CameraButtonClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
+            }
+            catch(Exception w)
+            {
+                throw new Exception(w.Message);
+            }
         }
 
 
