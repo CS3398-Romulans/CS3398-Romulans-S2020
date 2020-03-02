2b340ca4394c32b7db421f0447029ae17da34b2b
imarmolejo <60281875+imarmolejo@users.noreply.github.com>
2020-03-01 19:48:14 -0600

    Add options for sharing

---
+++ Frisbee/SharePage.xaml.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Frisbee
+{
+    public partial class SharePage : ContentPage
+    {
+        public SharePage()
+        {
+            InitializeComponent();
+        }
+
+        void BackButtonClicked(System.Object sender, System.EventArgs e)
+        {
+        }
+
+        void FACEBOOKButtonClicked(Object sender, EventArgs e)
+        {
+
+        }
+
+        void InstagramButtonClicked(Object sender, EventArgs e)
+        {
+
+        }
+
+        void TwitterButtonClicked(Object sender, EventArgs e)
+        {
+
+        }
+    }
+}
