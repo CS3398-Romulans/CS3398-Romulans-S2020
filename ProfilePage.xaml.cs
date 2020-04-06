6572cfc6f6230535c8f34ca2dbaec3abd1237800
imarmolejo <60281875+imarmolejo@users.noreply.github.com>
2020-04-06 18:13:23 -0500

    Add ProfilePage
    This commit adds the profile page but additional changes need to be made further.

---
+++ ProfilePage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Plugin.Media;
+using Xamarin.Forms;
+
+
+using Xamarin.Forms;
+
+namespace Frisbee
+{
+    public partial class ProfilePage : ContentPage
+    {
+        public ProfilePage()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
